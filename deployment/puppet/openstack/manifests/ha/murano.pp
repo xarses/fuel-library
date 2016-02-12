d6396525a1e6b8af25d034748fd99ea71433aed6
Andrey Bubyr <ab8675@att.com>
2016-02-12 15:45:56 -0600

    [9-Kilo] Re-enable haproxy config creation for murano rabbitmq
    Murano expects public vip as its own rabbitmq host address, so haproxy
    configuration creation for Murano Rabbitmq server was re-enabled

--- deployment/puppet/openstack/manifests/ha/murano.pp
+++ deployment/puppet/openstack/manifests/ha/murano.pp
@@ -90,18 +90,18 @@ class openstack::ha::murano (
 #    }
 #  }
 #
-#  openstack::ha::haproxy_service { 'murano_rabbitmq':
-#    order                  => '191',
-#    listen_port            => 55572,
-#    define_backups         => true,
-#    internal               => false,
-#    haproxy_config_options => {
-#      'option'         => ['tcpka'],
-#      'timeout client' => '48h',
-#      'timeout server' => '48h',
-#      'balance'        => 'roundrobin',
-#      'mode'           => 'tcp'
-#    },
-#    balancermember_options => 'check inter 5000 rise 2 fall 3',
-#  }
+  openstack::ha::haproxy_service { 'murano_rabbitmq':
+    order                  => '191',
+    listen_port            => 55572,
+    define_backups         => true,
+    internal               => false,
+    haproxy_config_options => {
+      'option'         => ['tcpka'],
+      'timeout client' => '48h',
+      'timeout server' => '48h',
+      'balance'        => 'roundrobin',
+      'mode'           => 'tcp'
+    },
+    balancermember_options => 'check inter 5000 rise 2 fall 3',
+  }
 }
